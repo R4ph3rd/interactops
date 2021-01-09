4fca42cd4ea4fc7f5ae851b219fe00f1401d5b7c
r4ph3rd <hav4na@protonmail.com>
2021-01-09 14:00:02 +0100

    update socket server

--- socket_network/public/socket.js
+++ socket_network/public/socket.js
@@ -4,6 +4,8 @@ const numberOfUsers = document.getElementById('users');
 
 console.log('SOcket server', numberOfUsers)
 
+numberOfUsers.innerText = users ;
+
 socket.on('update-users-list', data => {
     console.log(data);
     numberOfUsers.innerText = data.users.length;
