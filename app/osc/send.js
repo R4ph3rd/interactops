464f69c9bfef4a9dd2bb28d4679bbe7712f66973
r4ph3rd <hav4na@protonmail.com>
2021-01-11 16:01:04 +0100

    update wekinator config

--- app/osc/send.js
+++ app/osc/send.js
@@ -5,7 +5,7 @@ const client = new osc.Client('127.0.0.1', wekSendPort);
 
 module.exports = function(data){
     if (data.rotation && data.acceleration){
-        const message = new osc.Message('/companion');
+        const message = new osc.Message('/companion-inputs');
         message.append(data.rotation[0]);
         message.append(data.rotation[1]);
         message.append(data.rotation[2]);
@@ -13,6 +13,7 @@ module.exports = function(data){
         message.append(data.acceleration[1]);
         message.append(data.acceleration[2]);
 
+        console.log('message', message)
         client.send(message);
     } else {
         console.warn('Data format exported from the companion is not correct. Please provide acceleration & rotation datas.')
