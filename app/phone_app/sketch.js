485e9523f1395f7e6b16d86000abe55c15cce2ed
AaronVS <aaron.vicente@edu.uah.es>
2021-01-11 11:02:52 +0100

    acceleration Z test

--- app/phone_app/sketch.js
+++ app/phone_app/sketch.js
@@ -9,3 +9,12 @@ function draw(){
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight)
 }
+
+/* move the touchscreen of the device to register the acceleration changes ACC Z
+this is the one of the accelerations that suits the most in here
+*/
+function draw() {
+    background(220, 50);
+    fill('magenta'); //it can be any color
+    ellipse(width / 2, height / 2, accelerationZ);
+}
