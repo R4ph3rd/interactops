aeff4fb35a4eab67f90facfcf3152ad6d82e64e4
r4ph3rd <hav4na@protonmail.com>
2021-01-12 01:02:46 +0100

    update server

--- store/index.js
+++ store/index.js
@@ -4,7 +4,7 @@ module.exports = {
         temp: {}
     },
     archived: [],
-    tempDelay: 10000, //delay to store temporaly content shared by users
+    tempDelay: 20000, //delay to store temporaly content shared by users
     archivesClearInterval : 3600000, // every hour
     checkInterval : 60000, // every min
     checkTimeout : this.checkInterval/2,
