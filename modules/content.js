2bd7e247d56e8bd37eb3b0d00a6ef7609347116e
r4ph3rd <hav4na@protonmail.com>
2021-01-12 18:43:22 +0100

    sharing content

--- modules/content.js
+++ modules/content.js
@@ -1,11 +1,17 @@
 // const store = require('../store');
 
+const file = require("../app/actions/file");
+
 module.exports = function(io, socket, store){
     // content sharing
-	socket.on('share-content', ({data}) => {
+	socket.on('share-content', ({data, fileName}) => {
 		store.rooms.temp.socketId = socket.id;
-		store.rooms.temp.data = data
-        store.rooms.temp.requests = [];
+		store.rooms.temp.data = data;
+		store.rooms.temp.requests = [];
+
+		if (fileName){
+			store.rooms.temp.fileName = fileName;
+		}
 
         setTimeout( () => {
             store.archived.push(store.rooms.temp);
@@ -29,10 +35,18 @@ module.exports = function(io, socket, store){
 		if (store.rooms.temp.data){
 			store.rooms.temp.requests.push(socket.id);
 		
-			socket.emit('get-content', {
-				content : store.rooms.temp.data,
-				temp: store.rooms.temp
-			})
+			if (store.rooms.temp.fileName){
+				socket.emit('get-content', {
+					content : store.rooms.temp.data,
+					fileName: store.rooms.temp.fileName,
+					socketId: store.rooms.temp.socketId
+				})
+			} else {
+				socket.emit('get-content', {
+					content : store.rooms.temp.data,
+					socketId: store.rooms.temp.socketId
+				})
+			}
 		} else {
 			socket.emit('send-message', {
 				message: 'The cache is empty.' + store.rooms
