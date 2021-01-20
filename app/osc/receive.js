03015def9155f9e1393a2bc74d1f21fb352a1035
r4ph3rd <hav4na@protonmail.com>
2021-01-20 14:41:03 +0100

    alt tab fix

--- app/osc/receive.js
+++ app/osc/receive.js
@@ -11,18 +11,17 @@ module.exports = () => {
     wekinatorServer.on('message', async function(msg, info){
       // here I get an array where msg[0] == "/[gesture]", and others are confidence value of each model
 
-      if (store.altTab && msg[0] == ('/swipe-left' || 'swipe-right')){
+      if (store.altTab && ['/swipe-left', '/swipe-right'].includes(msg[0])){
         console.log('Alt tab : '.yellow , msg[0])
-
-        if (filters.lastRecognizedGesture == '/access-collaborator' && msg[0] == '/swipe-right'){
+        filters.lastRecognizedGesture = msg[0];
+        
+      } else if(!store.altTab && msg.length < 2){   
+          if (filters.lastRecognizedGesture == '/access-collaborator' && msg[0] == '/swipe-right'){
           return;
         } else {
           filters.lastRecognizedGesture = msg[0];
         }
-
-      } else if(!store.altTab && msg.length < 2){   
         console.log('Recognized gesture : '.yellow , msg[0])
-        filters.lastRecognizedGesture = msg[0];
       }
     })
 }
